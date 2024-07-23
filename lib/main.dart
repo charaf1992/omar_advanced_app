0d9aa087dc59730422374203598e39a4958d5e9d
charaf1992 <aouadi.charfeddine@outlook.com>
2024-07-23 08:27:21 +0100

    few syntax changes to match Omar's course

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-// https://www.youtube.com/watch?v=QxEGWJx_Vyo&list=PLwWuxCLlF_ucnfkI-_yNRCOTI-yJa5N-a
-// 59:48
+// https://www.youtube.com/watch?v=nncy2ccjDLk&list=PLwWuxCLlF_ucnfkI-_yNRCOTI-yJa5N-a&index=9
+// 00:00
 
 import 'package:flutter/material.dart';
 import 'package:omar_advanced_course/core/routing/app_router.dart';
